b296093c2b757891dac752ed6679efdfb05fa1f7
Alvin Galit <aroblesgalit@gmail.com>
2020-04-08 21:05:41 -0500

    Added prompts for title and description

--- genREADME.md
+++ genREADME.md
@@ -1,2 +1,5 @@
-Username: aroblesgalit
+# Good README Generator
+                > A command-line application that dynamically generates a README file.
+                Username: aroblesgalit
                 ![User Profile Image](https://avatars1.githubusercontent.com/u/38934010?v=4)
+                
--- index.js
+++ index.js
@@ -3,36 +3,49 @@ const axios = require("axios");
 const inquirer = require("inquirer");
 
 inquirer
-    .prompt({
-        message: "Enter your GitHub username:",
-        name: "username"
-    })
-    .then(function({ username }) {
+    .prompt([
+        {
+            message: "Enter your GitHub username:",
+            name: "username"
+        },
+        {
+            message: "What's the title for this repository?",
+            name: "title"
+        },
+        {
+            message: "Enter a description:",
+            name: "description"
+        }
+    ])
+    .then(function ({ username, title, description }) {
 
         const queryUrl = `https://api.github.com/search/users?q=${username}`;
 
         axios
             .get(queryUrl)
-            .then(function(response) {
+            .then(function (response) {
                 // console.log(response.data.items[0]);
                 const data = response.data.items[0];
                 const { avatar_url } = data;
                 // console.log(avatar_url);
 
-                const readMeLayout = `Username: ${username}
-                ![User Profile Image](${avatar_url})`;
-                
-                fs.writeFile("genREADME.md", readMeLayout, function(err) {
+                const readMeLayout = `# ${title}
+                > ${description}
+                Username: ${username}
+                ![User Profile Image](${avatar_url})
+                `;
+
+                fs.writeFile("genREADME.md", readMeLayout, function (err) {
                     if (err) {
                         return console.log(err);
                     }
 
                     console.log(`README generated.`);
                 })
-            
+
             })
-            .catch(function(err) {
-                console.log(err);            
+            .catch(function (err) {
+                console.log(err);
             });
 
     });
